e7675044f5652f4bbe7f887f62a08d5dd063c34e
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2025-03-19 15:23:29 +0100

    🎨 First JavaScript Support

---
+++ script.js
@@ -0,0 +1,27 @@
+const yourName = prompt("Quel est votre prénom ?");
+
+// ? Premier exercice
+
+// Clique sur le fichier HTML, puis clique sur "Go Live".
+// Dans ton navigateur, tu constateras que le prompt n'apparaît pas dans le navigateur.
+// Tu vas devoir identifier et régler ce problème 😊
+
+// ? Deuxième exercice
+
+// Tu vas devoir lancer une alerte JavaScript seulement si l'utilisateur a renseigné une valeur dans l'input !
+// Sinon, tu peux renvoyer un message de bienvenue avec le nom renseigné dans l'input 😊
+
+// ? Troisième exercice
+
+// Tu vas créer un prompt qui permettra à l'utilisateur de renseigner son âge (une valeur numérique est attendue).
+
+// ? Quatrième exercice
+
+// Tu vas vérifier si l'âge est supérieur ou égale à 18 ans.
+// Si tel est le cas, tu renverras une alerte déclarant : "Tu es majeur !"
+// Si tel n'est pas le cas, tu renverras une alerte déclarant : "Tu es mineur !".
+
+// ? Cinquième exercice
+
+// Tu vas créer une boucle dont le but qui exécutera cinq fois une alerte.
+// La dernière alerte apparente devrait afficher le texte suivant : "Tour de boucle n°4".
